75648a752288acd616d869050ae76d340ccc4729
Ermix 10One <ermanno.vuano@gmail.com>
2024-05-27 08:37:31 +0000

    prove in classe

---
+++ Progett1_cpp/progetto_27.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using namespace::std;
+
+int main(){
+
+    cout<<"Benvenuto su GitPod"<<endl;
+    string name;
+    cout<<"Scrivi il tuo nome: ";
+    cin>>name;
+    cout<<"Ciao, "<<name<<endl;
+
+    return 0;
+}
